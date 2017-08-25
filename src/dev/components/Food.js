e59ac796260c6ae76a8f5f93712d5389496f269c
Sebastian Nyberg <sebastian@pandapropaganda.com>
2017-08-25 10:56:11 +0300

    Food.js minor bug

--- src/dev/components/Food.js
+++ src/dev/components/Food.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import setHTMLBackground from '../functions/setHTMLBackground.js';
 import Menu from './Menu.js';
+import TextHeader from './TextHeader.js';
 
 const Food = (props) => {
     setHTMLBackground(0);
